3b82463be0fabc8767c7df19b5194f93ba05b855
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-02-29 14:02:49 +0200

    wip 1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,4 +18,10 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/push-ups/',
+  plugins: [react(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Push Ups',
+        short_name: 'Push Ups',
+        description: 'Push Ups personal',
+        theme_color: '#49f65d',
+        display: 'standalone',
+      }
+
+
+  })],
 })
