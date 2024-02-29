b836b9a6c18bbae106e5a0a7a7c31635ba2512aa
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-02-29 14:56:01 +0200

    minimal css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import classes from './app.module.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
-    </>
+    <div className={classes.root}>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+    </div>
   )
 }
 
