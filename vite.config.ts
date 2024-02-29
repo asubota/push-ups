48bf496cb1b3562283fc62fd8990ffd21d71a19c
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-02-29 14:48:33 +0200

    add prettier & clean-up

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/push-ups/',
-  plugins: [react(),
+  plugins: [
+    react(),
     basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
@@ -18,8 +18,7 @@ export default defineConfig({
         description: 'Push Ups personal',
         theme_color: '#49f65d',
         display: 'standalone',
-      }
-
-
-  })],
+      },
+    }),
+  ],
 })
