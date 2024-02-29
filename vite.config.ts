91f1a3d0fc0f9ef50da6ff19c0ce2bf1a9d53e8d
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-02-29 16:14:31 +0200

    correct images path

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
+const base = '/push-ups'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/push-ups/',
+  base,
   plugins: [
     react(),
     basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
 
       manifest: {
         name: 'Push Ups',
@@ -19,19 +24,35 @@ export default defineConfig({
         theme_color: '#49f65d',
         background_color: '#ff7474',
         display: 'standalone',
+        screenshots: [
+          {
+            src: `${base}/screenshot-640-320.png`,
+            sizes: '640x320',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'Push Ups',
+          },
+          {
+            src: `${base}/screenshot-320-640.png`,
+            sizes: '320x640',
+            type: 'image/png',
+            form_factor: 'narrow',
+            label: 'Push Ups',
+          },
+        ],
         icons: [
           {
-            src: '/icon-144.png',
+            src: `${base}/icon-144.png`,
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: `${base}/icon-192.png`,
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512.png',
+            src: `${base}/icon-512.png`,
             sizes: '512x512',
             type: 'image/png',
           },
