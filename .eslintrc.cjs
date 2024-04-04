f6a79e716629dee30c0ac8b66eb94aed9c6fbe4d
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-04-04 23:49:04 +0300

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
@@ -11,7 +16,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
