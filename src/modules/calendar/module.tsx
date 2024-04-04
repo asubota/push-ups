2b57cd54acf2f5a7be4faaa9a69370bfc703febb
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-04-04 23:42:29 +0300

    add mui calender

--- src/modules/calendar/module.tsx
+++ src/modules/calendar/module.tsx
@@ -1,5 +1,11 @@
 import { FC } from 'react'
+import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export const CalendarModule: FC = () => {
-  return <div></div>
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+    </LocalizationProvider>
+  )
 }
